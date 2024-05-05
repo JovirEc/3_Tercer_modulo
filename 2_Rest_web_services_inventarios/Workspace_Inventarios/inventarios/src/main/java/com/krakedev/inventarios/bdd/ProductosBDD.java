115d43947ff4ab05c20f6fe2a53cd265b7980ba5
Jovir69 <jovirodriguezgye@gmail.com>
2024-05-05 00:43:49 -0500

    Clase ProductosBDD terminada

---
+++ 2_Rest_web_services_inventarios/Workspace_Inventarios/inventarios/src/main/java/com/krakedev/inventarios/bdd/ProductosBDD.java
@@ -0,0 +1,90 @@
+package com.krakedev.inventarios.bdd;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.krakedev.inventarios.entidades.Categoria;
+import com.krakedev.inventarios.entidades.Producto;
+import com.krakedev.inventarios.entidades.UnidadDeMedida;
+import com.krakedev.inventarios.excepciones.KrakeDevException;
+import com.krakedev.inventarios.utils.ConexionBDD;
+
+public class ProductosBDD {
+	
+	public ArrayList<Producto> buscar(String subcadena) throws KrakeDevException{
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Producto producto;
+		
+		try {
+			con = ConexionBDD.obtenerConexion();
+			ps = con.prepareStatement("select prod.codigo_prod, prod.nombre as nombre_prod, "
+					+ "udm.codigo_udm, udm.descripcion as descripcion_udm, "
+					+ "cast (prod.precio_venta as decimal (5,2)), prod.tiene_iva, cast (prod.coste as decimal (5,2)), "
+					+ "cat.codigo_cat, cat.nombre as nombre_cat, prod.stock "
+					+ "from productos prod, unidades_medida udm, categorias cat "
+					+ "where prod.udm = udm.codigo_udm "
+					+ "and prod.categoria = cat.codigo_cat "
+					+ "and upper (prod.nombre) like ?");
+			ps.setString(1, subcadena);
+			
+			rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				
+				int codigoProd = rs.getInt("codigo_prod");
+				String nombreProd = rs.getString("nombre_prod");
+				String codigoUDM = rs.getString("codigo_udm");
+				String descripcionUDM = rs.getString("descripcion_udm");
+				BigDecimal precioVenta = rs.getBigDecimal("precio_venta");
+				boolean tieneIva = rs.getBoolean("tiene_iva");
+				BigDecimal coste = rs.getBigDecimal("coste");
+				int codigoCat = rs.getInt("codigo_cat");
+				String nombreCat = rs.getString("nombre_cat");
+				int stock = rs.getInt("stock");		
+				
+				UnidadDeMedida unidadDeMedida = new UnidadDeMedida();
+				unidadDeMedida.setCodigoUdm(codigoUDM);
+				unidadDeMedida.setDescripcion(descripcionUDM);
+				
+				Categoria categoria = new Categoria();
+				categoria.setCodigoCat(codigoCat);
+				categoria.setNombre(nombreCat);
+				
+				producto = new Producto();
+				producto.setCodigoProd(codigoProd);
+				producto.setNombre(nombreProd);
+				producto.setUnidadMedida(unidadDeMedida);
+				producto.setPrecioVenta(precioVenta);
+				producto.setTieneIva(tieneIva);
+				producto.setCoste(coste);
+				producto.setCategoria(categoria);
+				producto.setStock(stock);
+				
+				productos.add(producto);
+
+			}	
+			
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeDevException("Error al consultar base de datos. Detalles: "+e.getMessage());
+		}
+		
+		return productos;
+
+	}
+
+	
+	
+
+	
+}
