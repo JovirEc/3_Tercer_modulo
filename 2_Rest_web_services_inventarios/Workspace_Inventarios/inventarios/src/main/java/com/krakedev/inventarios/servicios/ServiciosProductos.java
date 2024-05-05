f2c2a75ad349c93c11391835489d8520665a0616
Jovir69 <jovirodriguezgye@gmail.com>
2024-05-05 13:10:11 -0500

    Reto 11 completo

--- 2_Rest_web_services_inventarios/Workspace_Inventarios/inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosProductos.java
+++ 2_Rest_web_services_inventarios/Workspace_Inventarios/inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosProductos.java
@@ -2,7 +2,9 @@ package com.krakedev.inventarios.servicios;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -35,4 +37,42 @@ public class ServiciosProductos {
 
 	}
 	
+	@Path("crear")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response insertar(Producto producto) {
+		ProductosBDD prodBDD = new ProductosBDD();
+		
+		try {
+			prodBDD.insertar(producto);
+			return Response.ok().build();
+			
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
