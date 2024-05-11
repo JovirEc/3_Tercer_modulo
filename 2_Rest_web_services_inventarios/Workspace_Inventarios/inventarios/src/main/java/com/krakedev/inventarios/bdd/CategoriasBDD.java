278fdff9797c03934de4b9ef3dfad35883fff914
Jovir69 <jovirodriguezgye@gmail.com>
2024-05-11 12:14:19 -0500

    Avance de Recuperar Categorias

--- 2_Rest_web_services_inventarios/Workspace_Inventarios/inventarios/src/main/java/com/krakedev/inventarios/bdd/CategoriasBDD.java
+++ 2_Rest_web_services_inventarios/Workspace_Inventarios/inventarios/src/main/java/com/krakedev/inventarios/bdd/CategoriasBDD.java
@@ -2,8 +2,10 @@ package com.krakedev.inventarios.bdd;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.ArrayList;
 
 import com.krakedev.inventarios.entidades.Categoria;
 import com.krakedev.inventarios.excepciones.KrakeDevException;
@@ -35,5 +37,75 @@ public class CategoriasBDD {
 			e.printStackTrace();
 			throw new KrakeDevException ("Error al crear categoria. Detalles: "+e.getMessage());
 		}
+	}
+	
+	
+	public void actualizar(Categoria categoria) throws KrakeDevException {
+		Connection con;
+		PreparedStatement ps;
+		try {
+			con = ConexionBDD.obtenerConexion();
+			ps = con.prepareStatement("update categorias set nombre = ? , set categoria_padre = ? ; "
+					+ "where codigo_cat = ?");
+			
+			ps.setString(1, categoria.getNombre());
+			if(categoria.getCategoriaPadre() == null) {
+				ps.setNull(2, Types.INTEGER);
+			}else {
+				ps.setInt(2, categoria.getCategoriaPadre().getCodigoCat());
+			}
+			ps.setInt(3, categoria.getCodigoCat());
+			
+			ps.executeUpdate();
+			
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			throw e;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakeDevException ("Error al actualizar categoria. Detalles: "+e.getMessage());
+			
+		}
+		
+	}
+	
+	public ArrayList<Categoria> recuperar(){
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ArrayList<Categoria> listaCategorias = new ArrayList<Categoria>();
+		Categoria categoriaX;
+
+		try {
+			con = ConexionBDD.obtenerConexion();
+			ps = con.prepareStatement("select codigo_cat,nombre,categoria_padre from categorias");
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				int codigoCat = rs.getInt("codigo_cat");
+				String nombre = rs.getString("nombre");
+				Categoria categoriaPadre = new Categoria();
+					int codigoCategoriaPadre = rs.getInt("categoria_padre");
+					if(codigoCategoriaPadre == 0) {
+						categoriaPadre = null;
+					}else {
+						categoriaPadre.setCodigoCat(codigoCategoriaPadre);
+					}
+				categoriaX = new Categoria(codigoCat,nombre,categoriaPadre);
+				listaCategorias.add(categoriaX);
+			}
+			
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		return null;
 	}
 }
--- 2_Rest_web_services_inventarios/Workspace_Inventarios/inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosCategorias.java
+++ 2_Rest_web_services_inventarios/Workspace_Inventarios/inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosCategorias.java
@@ -2,6 +2,7 @@ package com.krakedev.inventarios.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -27,5 +28,20 @@ public class ServiciosCategorias {
 			e.printStackTrace();
 			return Response.serverError().build();
 		}
+	}
+	
+	@Path("actualizar")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response actualizar(Categoria categoria) {
+		CategoriasBDD catBDD = new CategoriasBDD();
+		try {
+			catBDD.actualizar(categoria);
+			return Response.ok().build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+
 	}
 }
