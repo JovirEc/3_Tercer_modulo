6fff71afb91833118bc0b1d81954d37d393d6e3e
Joel Villamar <jovirodriguezgye@gmail.com>
2024-06-03 23:59:47 -0500

    Reto Repaso Parte 2 completada

--- Repaso Modulo 3/Reto 37/consumoRestWS/services/TestServices.js
+++ Repaso Modulo 3/Reto 37/consumoRestWS/services/TestServices.js
@@ -1,6 +1,6 @@
 
 export const getAllPostsService = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => {return response.json()})
-    .then((json) => {return console.log(json)})
+    .then((response) => response.json())
+    .then((json) => console.log(json))
 }
