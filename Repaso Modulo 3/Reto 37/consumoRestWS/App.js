5d38c74317a6ab2c0b7053105d813f4a522d6c20
Joel Villamar <jovirodriguezgye@gmail.com>
2024-06-04 22:16:52 -0500

    Reto Repaso 2 - Parte 1

--- Repaso Modulo 3/Reto 37/consumoRestWS/App.js
+++ Repaso Modulo 3/Reto 37/consumoRestWS/App.js
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import {TestWebServices} from './screens/TestWebServices'
+import { PostForm } from './screens/PostForm'
 export default function App() {
   const StackProducts = createNativeStackNavigator(); 
   return (
     <NavigationContainer>
-      <StackProducts.Navigator>
+      <StackProducts.Navigator initialRouteName='PostFormView'>
         <StackProducts.Screen name="TestWebServicesNav"  component={TestWebServices}/>
+        <StackProducts.Screen name="PostFormView"  component={PostForm} options={{title: 'MENSAJES'}}/>
       </StackProducts.Navigator>
     </NavigationContainer>
   );
